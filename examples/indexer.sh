4aceaf79b8be4b091251e3d3600ee3c3e1fb22cb
dakusui <dakusui@gmail.com>
2015-02-27 22:03:10 +0900

    Calibrate indexer exmple

--- examples/indexer.sh
+++ examples/indexer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find /home/hiroshi/Documents/IdeaProjects/jcunit/ -type f -name '*.java' |\
-cat -n |pv |\
+find ${1:-$(pwd)} -type f -name ${2:-"*.txt"} |\
+cat -n |pv | tee docid.txt |\
 bred -c 3 -M map -j 0 -I 'awk' -r '{
   l=1;
   while(( getline line<$2) > 0 ) {
@@ -13,8 +13,8 @@ bred -c 3 -M map -j 0 -I 'awk' -r '{
      }
      l++;
   }
-}' |pv |\
-bred -c 3 -s '-n' -O no -M reduce -j 1 -I 'awk' -r 'BEGIN {
+}' |pv | tee terms.txt |\
+bred -c 3 -s 4 -O no -M reduce -j 1 -I 'awk' -r 'BEGIN {
     p=""
     key=""
 }
