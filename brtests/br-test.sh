a4a2536e3b1bcbf4f7eaea2b24b76716b7612b0d
dakusui <dakusui@gmail.com>
2015-02-28 19:26:17 +0900

    Clean up the code

--- brtests/br-test.sh
+++ brtests/br-test.sh
@@ -6,17 +6,17 @@ function exec_test() {
     NUM_TOTAL=$(($NUM_TOTAL + 1))
     local testcasename=$1
     if [ $(declare -F "$testcasename" | wc -l) != "1" ]; then
-	echo "TESTRESULT:$testcasename:FAILED:Undefined"
+        echo "TESTRESULT:$testcasename:FAILED:Undefined"
         return 1
     fi
     local expected=$("${testcasename}" 'expected')
     local actual=$("${testcasename}" 'actual')
-    local result=$(diff <(echo $expected) <(echo $actual))
+    local result=$(diff <(echo "$expected") <(echo "$actual"))
     if [ $? == 0 ] && [ -z "$result" ]; then
         NUM_PASSED=$(($NUM_PASSED + 1))
-	echo "TESTRESULT:$testcasename:PASS"
+        echo "TESTRESULT:$testcasename:PASS"
     else
-	echo "TESTRESULT:$testcasename:FAIL:$?:$result"
+        echo "TESTRESULT:$testcasename:FAIL:$?:$result"
         echo "* EXPECTED:$expected"
         echo "* ACTUAL  :$actual"
     fi
@@ -24,9 +24,9 @@ function exec_test() {
 
 function bredcheckenv_nc_is_installed() {
     if [ $1 == 'expected' ]; then
-	echo '1'
+        echo '1'
     elif [ $1 == 'actual' ]; then
-	which nc | wc -l 
+        which nc | wc -l 
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -35,9 +35,9 @@ function bredcheckenv_nc_is_installed() {
 
 function bredcheckenv_nc_supports_listen_mode() {
     if [ $1 == 'expected' ]; then
-	echo '2'
+        echo '2'
     elif [ $1 == 'actual' ]; then
-        nc -h |& grep -e "-l" | grep listen | wc -l
+        nc -h 2>&1 | grep -e "-l" | grep listen | wc -l
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -46,9 +46,9 @@ function bredcheckenv_nc_supports_listen_mode() {
 
 function bredcheckenv_pv_is_nstalled() {
     if [ $1 == 'expected' ]; then
-	echo '1'
+        echo '1'
     elif [ $1 == 'actual' ]; then
-	which pv | wc -l 
+        which pv | wc -l 
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -57,9 +57,9 @@ function bredcheckenv_pv_is_nstalled() {
 
 function bredcheckenv_awk_is_installed() {
     if [ $1 == 'expected' ]; then
-	echo '1'
+        echo '1'
     elif [ $1 == 'actual' ]; then
-	which awk | wc -l 
+        which awk | wc -l 
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -68,9 +68,9 @@ function bredcheckenv_awk_is_installed() {
 
 function bredcheckenv_passwordless_ssh_is_ok() {
     if [ $1 == 'expected' ]; then
-	echo 'hello'
+        echo 'hello'
     elif [ $1 == 'actual' ]; then
-	timeout 5 ssh localhost echo "hello"
+        timeout 5 ssh localhost echo "hello"
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -81,7 +81,7 @@ function bredcheckenv_brp_is_installed() {
     if [ $1 == 'expected' ]; then
         echo '1'
     elif [ $1 == 'actual' ]; then
-	which brp | wc -l
+        which brp | wc -l
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -93,7 +93,7 @@ function bredcheckenv_brp_is_installed() {
 # verify reducer is working
 function bredtest_1reducer() {
     if [ $1 == 'expected' ]; then
-	echo '0 BEGIN
+        echo '0 BEGIN
 0 BEGIN
 0 BEGIN
 1 MAIN 01 B
@@ -104,8 +104,8 @@ function bredtest_1reducer() {
 2 MAIN 01 C
 3 END'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost" -j 0 -M reduce -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}'  2>/dev/null
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost" -j 0 -M reduce -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}'  2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -116,13 +116,13 @@ function bredtest_1reducer() {
 # verify 2 reducers are working
 function bredtest_2reducers() {
     if [ $1 == 'expected' ]; then
-	echo '1 MAIN 01 B z
+        echo '1 MAIN 01 B z
 1 MAIN 02 A zz
 1 MAIN 03 A x
 2 MAIN 01 C y'
     elif [ $1 == 'actual' ]; then
-	printf "03 A x\n02 A zz\n01 B z\n01 C y\n" | \
-	"$SUT" -m "localhost localhost" -j 0 -M reduce -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null | grep "MAIN" | sort
+        printf "03 A x\n02 A zz\n01 B z\n01 C y\n" | \
+        "$SUT" -m "localhost localhost" -j 0 -M reduce -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null | grep "MAIN" | sort
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -133,13 +133,13 @@ function bredtest_2reducers() {
 # verify 2 reducers are working with the thrid column as key
 function bredtest_2reducersK3() {
     if [ $1 == 'expected' ]; then
-	echo '1 MAIN 03 A x
+        echo '1 MAIN 03 A x
 1 MAIN 01 C y
 1 MAIN 01 B z
 1 MAIN 02 A zz'
     elif [ $1 == 'actual' ]; then
-	printf "03 A x\n02 A zz\n01 B z\n01 C y\n" | \
-	"$SUT" -c 3 -m "localhost localhost" -j 0 -M reduce -s 5 -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}'  2>/dev/null | grep "MAIN"
+        printf "03 A x\n02 A zz\n01 B z\n01 C y\n" | \
+        "$SUT" -c 3 -m "localhost localhost" -j 0 -M reduce -s 5 -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}'  2>/dev/null | grep "MAIN"
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -150,15 +150,15 @@ function bredtest_2reducersK3() {
 # verify mapper is working
 function bredtest_1mapper() {
     if [ $1 == 'expected' ]; then
-	echo '0 BEGIN
+        echo '0 BEGIN
 1 MAIN 03 A
 2 MAIN 02 A
 3 MAIN 01 B
 4 MAIN 01 C
 5 END'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost" -j 0 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost" -j 0 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -169,7 +169,7 @@ function bredtest_1mapper() {
 # verify 2 mappers are working
 function bredtest_2mappers() {
     if [ $1 == 'expected' ]; then
-	echo '0 BEGIN
+        echo '0 BEGIN
 0 BEGIN
 1 MAIN 02 A
 1 MAIN 03 A
@@ -178,8 +178,8 @@ function bredtest_2mappers() {
 3 MAIN 01 C
 4 END'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost localhost" -j 0 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost localhost" -j 0 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -190,13 +190,13 @@ function bredtest_2mappers() {
 # verify default sort
 function bredtest_default_sort_default_column() {
     if [ $1 == 'expected' ]; then
-	echo '01 B
+        echo '01 B
 01 C
 02 A
 03 A'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost localhost" -j 0 2>/dev/null
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost localhost" -j 0 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -207,13 +207,13 @@ function bredtest_default_sort_default_column() {
 # verify default sort
 function bredtest_default_sort_2nd_column() {
     if [ $1 == 'expected' ]; then
-	echo '02 A
+        echo '02 A
 03 A
 01 B
 01 C'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost localhost" -j 0 -c 2 2>/dev/null
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost localhost" -j 0 -c 2 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -224,14 +224,14 @@ function bredtest_default_sort_2nd_column() {
 # verify 2 mappers are working with the third column specified as a key
 function bredtest_2mappersK2() {
     if [ $1 == 'expected' ]; then
-	echo '1 MAIN 01 B
+        echo '1 MAIN 01 B
 1 MAIN 03 A
 2 MAIN 02 A
 3 MAIN 01 C
-' | sort -k 1,4
+'
     elif [ $1 == 'actual' ]; then
-	printf "03 A\n02 A\n01 B\n01 C\n" | \
-	"$SUT" -m "localhost localhost" -j 0 -c 2 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null | grep MAIN | sort -k 1,4
+        printf "03 A\n02 A\n01 B\n01 C\n" | \
+        "$SUT" -m "localhost localhost" -j 0 -c 2 -M map -I awk -r 'BEGIN{i=0;print i++,"BEGIN";} //{print i++,"MAIN",$0;} END{print i++,"END";}' 2>/dev/null | grep MAIN | sort -k 1,4
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -242,13 +242,13 @@ function bredtest_2mappersK2() {
 # verify word count works with compat mode
 function bredtest_compat_wordcount() {
     if [ $1 == 'expected' ]; then
-	echo '      1 everyone
+        echo '      1 everyone
       3 hello
       1 one
       2 world'
     elif [ $1 == 'actual' ]; then
-	printf "hello\nworld\neveryone\nhello\nhello\none\nworld" | \
-	"$SUT" -m "localhost" -r 'uniq -c' 2>/dev/null
+        printf "hello\nworld\neveryone\nhello\nhello\none\nworld" | \
+        "$SUT" -m "localhost" -r 'uniq -c' 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -259,13 +259,13 @@ function bredtest_compat_wordcount() {
 # verify word count works with compat mode (2 reducers)
 function bredtest_compat_wordcount_with_2reducers() {
     if [ $1 == 'expected' ]; then
-	echo '      1 everyone
+        echo '      1 everyone
       3 hello
       1 one
       2 world'
     elif [ $1 == 'actual' ]; then
-	printf "hello\nworld\neveryone\nhello\nhello\none\nworld" | \
-	"$SUT" -m "localhost localhost" -r 'uniq -c' 2>/dev/null
+        printf "hello\nworld\neveryone\nhello\nhello\none\nworld" | \
+        "$SUT" -m "localhost localhost" -r 'uniq -c' 2>/dev/null
     else
         echo "Invalid mode $1 is specified"
         exit 1
@@ -273,17 +273,24 @@ function bredtest_compat_wordcount_with_2reducers() {
 }
 
 DIRNAME=$(dirname $(dirname $0))
-SUT=$DIRNAME/bred
+SUT="$DIRNAME/bred"
 NUM_TOTAL=0
 NUM_PASSED=0
 
 function main() {
-    for each in $(declare -F | grep "bredcheckenv_" | cut -f 3 -d ' '); do
-       exec_test $each;
-    done
-    for each in $(declare -F | grep "bredtest_" | cut -f 3 -d ' '); do
-       exec_test $each;
-    done
+    if [[ $# > 0 ]]; then
+        for each in $@ ; do
+            exec_test "$each"
+        done
+    else
+        echo "Running all defined tests"
+        for each in $(declare -F | grep "bredcheckenv_" | cut -f 3 -d ' '); do
+            exec_test "$each";
+        done
+        for each in $(declare -F | grep "bredtest_" | cut -f 3 -d ' '); do
+            exec_test "$each";
+        done
+    fi
     echo "----"
     echo "TOTAL:$NUM_PASSED  / $NUM_TOTAL"
 }
--- examples/indexer.sh
+++ examples/indexer.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+#
+# Distributed indexing with bred.
+#
+# Usage:   indexer.sh [1: A directory from which traversal starts] [2: File pattern] [3: ndex file]
+#          1: Files under this directory are read and indexed
+#          2: Pattern to match files to be indexed
+#
+# Example: indexer.sh . '*.java' index.txt
+#          This script indexes all the '*.java' files under the current directory
+#          using the workers (hosts) listed in  /etc/br.hosts or ~/.br.hosts file.
+#
+
 eval dir=${1:-$(pwd)}
+index=${3:+"-o $3"}
 find $dir -type f -name ${2:-"*.txt"} |\
-cat -n |pv | tee docid.txt |\
-bred -c 3 -M map -j 0 -I 'awk' -r '{
+cat -n |pv | tee docid.txt | bred -c 3 -M map -j 0 -I 'awk' -r '{
   l=1;
   while(( getline line<$2) > 0 ) {
      gsub(/[[:punct:]]+/, " ", line);
@@ -14,20 +26,17 @@ bred -c 3 -M map -j 0 -I 'awk' -r '{
      }
      l++;
   }
-}' |pv | tee terms.txt |\
-bred -c 3 -s 4 -O no -M reduce -j 1 -I 'awk' -r 'BEGIN {
+}' |pv | tee terms.txt | bred -c 3 -s 4 -O no -M reduce -j 1 -I 'awk' -r 'BEGIN {
     p=""
     key=""
-}
-{
+} {
     if (key == "") {
         key=$3;
     }
     p=p " " $1 "," $2
-}
-END {
+} END {
     print "" key " [" ENVIRON["HOST_IDX"] "] " p;
-}' -o index.txt
+}' ${index}
 
 
 
